fc6dc01ded536da06b5fcfac9c0f8d1af42b7887
Daniel Muehlbachler-Pietrzykowski <daniel@muehlbachler.io>
2026-09-25 08:00:54 +0200

    fix: fix docusaurus warnings

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,8 +22,13 @@ const config = {
   projectName: 'hochschule-burgenland-docusaurus-demo', // Usually your repo name.
 
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
   trailingSlash: false,
+
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: 'warn',
+    }
+  },
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
